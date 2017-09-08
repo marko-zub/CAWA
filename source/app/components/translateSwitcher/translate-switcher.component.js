84c59abe21348a1beb9e816ea200ea604aae6270
marko <i.markozub@gmail.com>
2017-09-09 01:19:42 +0300

    init translate

---
+++ source/app/components/translateSwitcher/translate-switcher.component.js
@@ -0,0 +1,48 @@
+(function() {
+
+    'use strict';
+
+    angular
+        .module('app.components')
+        .controller('TranslateSwitcher', TranslateSwitcher)
+        .component('translateSwitcher', {
+            templateUrl: 'app/components/translateSwitcher/translate-switcher.html',
+            controller: 'TranslateSwitcher',
+            controllerAs: 'vm'
+        });
+
+    TranslateSwitcher.$inject = ['$translate'];
+
+    function TranslateSwitcher($translate) {
+        var vm = this;
+
+        vm.$onInit = onInit;
+
+        var LANGS = [
+        	{ name: 'UA', key: 'uk', selected: true},
+        	{ name: 'RUS', key: 'ru', selected: false},
+        	{ name: 'ENG', key: 'en', selected: false}
+        ];
+
+        function onInit() {
+            vm.langs = LANGS;
+            vm.isopen = false;
+            vm.langSelected = vm.langs[0];
+        }
+
+        vm.changeLanguage = changeLanguage;
+
+        function changeLanguage (index) {
+        	var prevIndex = _.findIndex(vm.langs, function (item) {
+        		return item.selected === true;
+        	});
+        	vm.langs[prevIndex].selected = false;
+
+        	vm.langSelected = vm.langs[index];
+        	vm.langs[index].selected = true;
+        	$translate.use(vm.langSelected.key);
+        }
+
+    }
+
+})();
---
+++ source/app/translate/translate.config.js
@@ -0,0 +1,37 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('pascalprecht.translate')
+		.config(['$translateProvider', function($translateProvider) {
+
+			$translateProvider.useStaticFilesLoader({
+				prefix: '/languages/',
+				suffix: '.json'
+			});
+
+			$translateProvider.translations('en', {
+				'DECISIONS': 'Decisions',
+				'USERS': 'Users',
+				'SIGN UP': 'Sign up',
+				'SEARCH': 'Search...'
+			});
+
+			$translateProvider.translations('ru', {
+				'DECISIONS': 'Решения',
+				'USERS': 'Пользователи',
+				'SIGN UP': 'Зарегистрироваться',
+				'SEARCH': 'Поиск...'
+			});
+
+			$translateProvider.translations('uk', {
+				'DECISIONS': 'Рішення',
+				'USERS': 'Користувачі',
+				'SIGN UP': 'Зареєструватися',
+				'SEARCH': 'Пошук...'
+			});
+
+			$translateProvider.preferredLanguage('uk');
+		}]);
+
+})();
